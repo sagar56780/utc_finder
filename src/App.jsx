e272ec9e0dbe4b19b04465188bb54ec1ac7dac7c
Your Name <you@example.com>
2024-07-09 00:37:32 +0100

    logics are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,61 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Select from "react-select";
 
 const App = () => {
-    let [countries,setContries]=useState([]);
+  let [countries, setContries] = useState([]);
+  let [selectedIndex, setIndex] = useState(-1);
+  let [timezones, settimezones] = useState([""]);
   useEffect(() => {
-    
     let fetch = async () => {
-      let {data} =await axios.get("https://restcountries.com/v3.1/all");
-        
-        setContries(data);
-        data.map((val,ind)=>{
-            val.map((val,ind)=>{
-               console.log(val);
-            })
-        })
-      
-    
+      let { data } = await axios.get("https://restcountries.com/v3.1/all");
+
+      data.sort((a, b) => {
+        const nameA = a.name.common.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.common.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+      setContries(data);
     };
     fetch();
-  
-      
-  
-      
-    
-  },[]);
+  }, []);
+  let handleClick = (e) => {
+    e.preventDefault();
+    settimezones(countries[e.target.value].timezones);
+    setIndex(e.target.value);
+    console.log(timezones);
+  };
 
   return (
-    <form action="">
+    <form action="" className="container">
       Enter country name:
-      <select>
-        {countries.map((val,ind)=>{
-            // return (<option key={ind} value={val.name}>{val.name}</option>)
+      <select id="country" className="form-select" onChange={handleClick}>
+        {countries.map((val, ind) => {
+          return (
+            <option value={ind} key={ind}>
+              {val.name.common}
+            </option>
+          );
         })}
       </select>
+      {timezones.map((val, ind) => {
+        return (
+          <>
+            <h1 key={ind}>{val}</h1>
+          </>
+        );
+      })}
+      <br />
+      <h1 className="mb-4">{selectedIndex>=0?countries[selectedIndex].capital:null}</h1>
+      <h1>{selectedIndex>0?countries[selectedIndex].region:null}</h1>
+       <img src={selectedIndex>0?countries[selectedIndex].flags.png:null} alt="" />
     </form>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// index.js
+import "bootstrap/dist/css/bootstrap.min.css";
 
-let root=createRoot(document.getElementById("root"));
+let root = createRoot(document.getElementById("root"));
 
 root.render(<App></App>);
